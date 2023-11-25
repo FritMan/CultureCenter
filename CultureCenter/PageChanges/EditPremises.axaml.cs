9306d45167c9fae65e5e1c0a81caee5182cb4578
Ансар <@DESKTOP-2QG5HPM>
2023-11-25 14:42:41 +0300

    Подгрузка помещений, добавление кнопок

---
+++ CultureCenter/PageChanges/EditPremises.axaml.cs
@@ -0,0 +1,77 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CultureCenter.Classes;
+using CultureCenter.data;
+using CultureCenter.Views;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using static CultureCenter.Classes.Helper;
+
+namespace CultureCenter.Pages;
+
+public partial class EditPremises : UserControl
+{
+    private long _id;
+    private Room Room;
+
+    public EditPremises()
+    {
+        InitializeComponent();
+        loadData();
+    }
+
+    public EditPremises(long id)
+    {
+        InitializeComponent();
+        _id = id;
+        OkBtn.Click += OkBtn_Click;
+        BackBtn.Click += BackBtn_Click;
+    }
+
+    private void BackBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        RefreshAll();
+        NavigationSystem.MainFrame.Content = new PremisesControl();
+    }
+    private void OkBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        try
+        {
+            if (_id == -1)
+            {
+                Db.Rooms.Add(Room);
+            }
+            Db.SaveChanges();
+            NavigationSystem.MainFrame.Content = new PremisesControl();
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+    }
+
+    private void loadData()
+    {
+        try
+        {
+            Db.Rooms.Load();
+
+            if (_id != -1)
+            {
+                Db.Rooms.Load();
+                Room = Db.Rooms.Where(el => el.Id == _id).First();
+            }
+            else
+            {
+                Room = new Room();
+            }
+            PremisesGrid.DataContext = Room;
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+    }
+}
--- CultureCenter/Pages/PremisesControl.axaml.cs
+++ CultureCenter/Pages/PremisesControl.axaml.cs
@@ -2,7 +2,13 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using CultureCenter.Classes;
+using CultureCenter.data;
 using CultureCenter.Views;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Data.Common;
+using System.Linq;
+using static CultureCenter.Classes.Helper;
 
 namespace CultureCenter.Pages;
 
@@ -12,10 +18,62 @@ public partial class PremisesControl : UserControl
     {
         InitializeComponent();
         BackBtn.Click += BackBtn_Click;
+        AddPremisesBtn.Click += AddPremisesBtn_Click; ;
+        EditPremisesBtn.Click += EditPremisesBtn_Click;
+        DeletePremisesBtn.Click += DeletePremisesBtn_Click;
+        SearchTB.TextChanged += SearchTB_TextChanged;
+        LoadData();
+    }
+
+    private void SearchTB_TextChanged(object? sender, TextChangedEventArgs e)
+    {
+        LoadData();
+    }
+
+    private void AddPremisesBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        NavigationSystem.MainFrame.Content = new EditPremises(-1);
+    }
+
+    private void EditPremisesBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var selectedRoom = PremisesDG.SelectedItem as Room;
+        if (selectedRoom != null)
+        {
+            NavigationSystem.MainFrame.Content = new EditPremises(selectedRoom.Id);
+        }
     }
 
     private void BackBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         NavigationSystem.MainFrame.Content = new MainView();
     }
+
+    private async void DeletePremisesBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var selectedRoom = PremisesDG.SelectedItem as Room;
+        if (selectedRoom != null)
+        {
+            if (await ShowQuestion("Вы уверены?") == MsBox.Avalonia.Enums.ButtonResult.Yes)
+            {
+                Db.Rooms.Remove(selectedRoom);
+                Db.SaveChanges();
+                NavigationSystem.MainFrame.Content = new EditPremises();
+            }
+        }
+    }
+
+    private void LoadData()
+    {
+        Db.Rooms.Load();
+        if (string.IsNullOrEmpty(SearchTB.Text))
+        {
+            PremisesDG.ItemsSource = Db.Rooms;
+        }
+        else
+        {
+            PremisesDG.ItemsSource = Db.Rooms.Where(el => el.Name.Contains(SearchTB.Text));
+        }
+    }
 }
+
