7c0449816bdcb333b3f895c753f1f0b45d35b394
Ансар <@DESKTOP-2QG5HPM>
2023-11-26 16:27:09 +0300

    Добавить рабочий стол, исправить кнопку ок , отригулировать эдит и всё будет классно

---
+++ CultureCenter/PageChanges/EditWorkOrders.axaml.cs
@@ -0,0 +1,96 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CultureCenter.Classes;
+using CultureCenter.data;
+using CultureCenter.Pages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+using System.Linq;
+using static CultureCenter.Classes.Helper;
+
+namespace CultureCenter.PageChanges;
+
+public partial class EditWorkOrders : UserControl
+{
+    private long _id;
+    private WorkOrder WorkOrder;
+    public EditWorkOrders()
+    {
+        InitializeComponent();
+        LoadData();
+    }
+
+    public EditWorkOrders(long id)
+    {
+        InitializeComponent();
+        LoadData();
+        _id = id;
+        OkBtn.Click += OkBtn_Click;
+        BackBtn.Click += BackBtn_Click;
+    }
+
+    private void BackBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        RefreshAll();
+        NavigationSystem.MainFrame.Content = new ApplicationControl();
+    }
+
+    private void OkBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        try
+        {
+            WorkOrder.DateStart = DateStartCmb.SelectedDate.Value.ToString("d");
+            WorkOrder.DateEnd = DateEndCmb.SelectedDate.Value.ToString("d");
+            if(_id == -1)
+            {
+                Db.WorkOrders.Add(WorkOrder);
+            }
+            Db.SaveChanges();
+            NavigationSystem.MainFrame.Content = new ApplicationControl();
+        }
+        catch(Exception ex)
+        {
+
+        }
+    }
+
+    private void LoadData()
+    {
+        try
+        {
+            Db.TypesOfEvents.Load();
+            Db.Events.Load();
+            Db.Rooms.Load();
+            Db.WorkOrders.Load();
+            Db.Statuses.Load();
+            Db.WorkTypes.Load();
+
+            EventCb.ItemsSource = Db.TypesOfEvents.ToList();
+            RoomCb.ItemsSource = Db.Rooms.ToList();
+            StatusCb.ItemsSource= Db.Statuses.ToList();
+            WorkTypesCb.ItemsSource = Db.WorkTypes.ToList();
+
+
+            if (_id == -1)
+            {
+                Db.WorkOrders.Load();
+
+                WorkOrder = Db.WorkOrders.Where(el => el.Id == _id).First();
+                DateStartCmb.SelectedDate = System.DateTime.Parse(WorkOrder.DateStart);
+                DateEndCmb.SelectedDate = System.DateTime.Parse(WorkOrder.DateEnd);
+            }
+            else
+            {
+                WorkOrder = new WorkOrder();
+            }
+            WorkOrderGrid.DataContext = WorkOrder;
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+}
--- CultureCenter/Pages/ApplicationControl.axaml.cs
+++ CultureCenter/Pages/ApplicationControl.axaml.cs
@@ -1,9 +1,16 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using CultureCenter.Classes;
+using CultureCenter.data;
+using CultureCenter.PageChanges;
 using CultureCenter.Views;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Linq;
+using static CultureCenter.Classes.Helper;
 
 namespace CultureCenter.Pages;
 
@@ -12,7 +19,85 @@ public partial class ApplicationControl : UserControl
     public ApplicationControl()
     {
         InitializeComponent();
+        LoadData();
         BackBtn.Click += BackBtn_Click;
+        ApplicationDG.LoadingRow += ApplicationDG_LoadingRow;
+        SearchTB.TextChanged += SearchTB_TextChanged;
+        AddApplicationBtn.Click += AddApplicationBtn_Click;
+        EditApplicationBtn.Click += EditApplicationBtn_Click;
+        DeleteApplicationBtn.Click += DeleteApplicationBtn_Click;
+    }
+
+    private async void DeleteApplicationBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        try
+        {
+            var selectedOrder = ApplicationDG.SelectedItem as WorkOrder;
+            if (selectedOrder != null)
+            {
+                if(await ShowQuestion("Вы уверены?") == MsBox.Avalonia.Enums.ButtonResult.Yes)
+                {
+                    Db.WorkOrders.Remove(selectedOrder);
+                    Db.SaveChanges();
+                    NavigationSystem.MainFrame.Content = new ApplicationControl();
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            
+        }
+    }
+
+    private void EditApplicationBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var selectedOrder = ApplicationDG.SelectedItem as WorkOrder;
+        if (selectedOrder != null)
+        {
+            NavigationSystem.MainFrame.Content = new EditWorkOrders(selectedOrder.Id);
+        }
+    }
+
+    private void AddApplicationBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        NavigationSystem.MainFrame.Content = new EditWorkOrders(-1);
+    }
+
+    private void SearchTB_TextChanged(object? sender, TextChangedEventArgs e)
+    {
+        LoadData();
+    }
+
+    private void ApplicationDG_LoadingRow(object? sender, DataGridRowEventArgs e)
+    {
+        var data = e.Row.DataContext as WorkOrder;
+
+        if (data.Status.Name == "Выполнена")
+        {
+            e.Row.Background = new SolidColorBrush(Colors.Gray);
+        }
+        if (data.Status.Name == "К выполнению")
+        {
+            e.Row.Background = new SolidColorBrush(Colors.Pink);
+        }
+    }
+
+    private void LoadData()
+    {
+        Db.TypesOfEvents.Load();
+        Db.WorkTypes.Load();
+        Db.Rooms.Load();
+        Db.Events.Load();
+        Db.WorkOrders.Load();
+        Db.Statuses.Load();
+        if (string.IsNullOrEmpty(SearchTB.Text))
+        {
+            ApplicationDG.ItemsSource = Db.WorkOrders;
+        }
+        else
+        {
+            ApplicationDG.ItemsSource = Db.WorkOrders.Where(el => el.Description.Contains(SearchTB.Text));
+        }
     }
 
     private void BackBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
