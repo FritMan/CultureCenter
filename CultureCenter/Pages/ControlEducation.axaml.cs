1dfe817a46b789a85b98ee3c2f80355ec6ef7155
Максим Астахов <mastahov112@gmail.com>
2023-11-26 23:39:34 +0300

    очень многа сделал

---
+++ CultureCenter/Pages/ControlEducation.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia.Controls;
+using CultureCenter.Classes;
+using CultureCenter.Views;
+
+namespace CultureCenter.Pages
+{
+    public partial class ControlEducation : UserControl
+    {
+        public ControlEducation()
+        {
+            InitializeComponent();
+            BackBtn.Click += BackBtn_Click;
+            EnlightenmentBtn.Click += EnlightenmentBtn_Click;
+        }
+
+        private void EnlightenmentBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            NavigationSystem.MainFrame.Content = new PremisesControl();
+        }
+
+        private void BackBtn_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            NavigationSystem.MainFrame.Content = new IntermediateMenu();
+        }
+    }
+}
