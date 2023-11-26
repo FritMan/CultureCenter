5eafd6c6ed51481aea3e2653ea0b98a121fa0ec8
Ансар <@DESKTOP-2QG5HPM>
2023-11-26 14:54:11 +0300

    Почти

---
+++ CultureCenter/PageChanges/EditWorkOrderes.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CultureCenter.PageChanges;
+
+public partial class EditWorkOrderes : UserControl
+{
+    public EditWorkOrderes()
+    {
+        InitializeComponent();
+    }
+}
--- CultureCenter/data/CultureDataBaseContext.cs
+++ CultureCenter/data/CultureDataBaseContext.cs
@@ -29,7 +29,7 @@ public partial class CultureDataBaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("data source =C:\\Users\\Ансар\\Documents\\RepProgiFinish\\CultureCenter\\DataBase\\CultureDataBase.db");
+        => optionsBuilder.UseSqlite("data source =C:\\Users\\Ансар\\Documents\\RepotizoriProgFinish\\CultureCenter\\DataBase\\CultureDataBase.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -61,6 +61,10 @@ public partial class CultureDataBaseContext : DbContext
         modelBuilder.Entity<WorkOrder>(entity =>
         {
             entity.ToTable("WorkOrder");
+
+            entity.Property(e => e.DateEnd).HasColumnType("TEXT (10)");
+            entity.Property(e => e.DateStart).HasColumnType("TEXT (10)");
+            entity.Property(e => e.Description).HasColumnType("TEXT (100)");
 
             entity.HasOne(d => d.Events).WithMany(p => p.WorkOrders)
                 .HasForeignKey(d => d.EventsId)
